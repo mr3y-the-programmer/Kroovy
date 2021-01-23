994c8f6080a0197969f80c06b7d881cc92bf63dc
MR3Y <abdonasr379@gmail.com>
2021-01-23 23:39:39 +0200

    Notify the user about the whole process progress

--- src/main/kotlin/com/mr3y/plugins/kroovy/ConvertAction.kt
+++ src/main/kotlin/com/mr3y/plugins/kroovy/ConvertAction.kt
@@ -19,11 +19,47 @@ package com.mr3y.plugins.kroovy
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.progress.ProgressIndicatorProvider
+import org.jetbrains.kotlin.com.intellij.openapi.diagnostic.Logger
 
 class ConvertAction: AnAction() {
 
+    private val logger by lazy {
+        Logger.getInstance("#com.intellij.openapi.actionSystem.ConvertAction")
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
-        TODO("Not yet implemented")
+        val progressIndicator = ProgressIndicatorProvider.getGlobalProgressIndicator()
+        val gradleFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
+        if (progressIndicator != null) {
+            PsiTransformer.transform(gradleFile)
+            when(val result = PsiTransformer.transformResult.value) {
+                is ConvertResult.Initial -> {
+                    logger.info("Psi Transformation is about to start!")
+                    progressIndicator.apply {
+                        isIndeterminate = false
+                        fraction = 0.0
+                        text = "Processing the file..."
+                    }
+                }
+                is ConvertResult.WorkInProgress -> {
+                    val progress = result.percentage
+                    logger.info("Psi Transformation has started!, Progress: $progress")
+                    progressIndicator.fraction = progress.toDouble().div(100).coerceIn(0.0, 1.0)
+                    progressIndicator.text = "Work In Progress..., Go walk around Or get yourself a Coffee"
+                }
+                is Finished.Succeeded -> {
+                    logger.info("Transformation Finished successfully!")
+                    progressIndicator.text = "We're Done, Take a look at your file in its new syntax"
+                }
+                is Finished.Failed -> {
+                    val reason = result.getMessage()
+                    logger.error(reason, result.exception ?: RuntimeException())
+                    progressIndicator.text = "Unfortunately, Transformation Failed:\nreason: $reason"
+                }
+            }
+        }
+        // TODO: represent the data in other visual way if progressIndicator isn't available
     }
 
     override fun update(e: AnActionEvent) {
